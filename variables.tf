041005b7dff4625c7c3c48e73c5f84ad5bb56b0f
Sundar Sudandiraraj <sundarsudandiraraj@airasia.com>
2021-11-17 15:00:42 +0800

    Add variable to configure Endpoint-Independent Mapping for NAT

--- variables.tf
+++ variables.tf
@@ -66,3 +66,9 @@ variable "nat_min_ports_per_vm" {
   type        = number
   default     = 64
 }
+
+variable "nat_enable_endpoint_independent_mapping" {
+  type        = bool
+  description = "Specifies if endpoint independent mapping is enabled. See https://cloud.google.com/nat/docs/overview#specs-rfcs"
+  default     = false
+}
